d95b77df32f182bc0f058b831fff2601a0d8383c
stankoua <s.tankouat@gmail.com>
2013-01-12 18:12:34 +0100

    On branch parser  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   Makefile
    	modified:   gestion_client.c
    	modified:   main
    	modified:   main.c
    	modified:   workflow_struct.h
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	../../PSR.tar
    	../../Test/SAX/myressax
    	../../Test/SAX/process2.xml
    	../../Test/SAX/test
    	../../Test/SAX/testressax2
    	../Workflow_admin/admin.c
    	../../rapport.odt

--- psr_wf/Workflow_engine/main.c
+++ psr_wf/Workflow_engine/main.c
@@ -1,52 +1,61 @@
 /* Programme principal du moteur */
 
-#include "optionServer.h"
-#include "gestion_client.h"
 #include <libsck.h>
 #include <libthrd.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "optionServer.h"
+#include "gestion_client.h"
+#include "workflow_struct.h"
 // stdio.h amené par gestion_client.h
 
 
 int main(int argc, char *argv[])
 {
-        short int nport=4000;          // nport num port utilisé pour la connexion
-        int ecoute;                    // Descripteur de la socket d'ecoute
-        char *ufname;
-        
+  short int nport=4000;          // nport num port utilisé pour la connexion
+  int ecoute;                    // Descripteur de la socket d'ecoute
+  char *ufname, uxml[SIZE];
 
-        ufname = (char*)malloc(sizeof "users");
-        strcpy(ufname,"users");
 
-	/* Lecture des arguments de la commande */
-        #ifdef DEBUG
-        printf("======= Lecture ======");
-        #endif
-        
-        // On verifie que le arguments sont bons
-        if((analyseArg(argc,argv,&nport,ufname))==-1)
-                {display_right_syntax(); return -1;}
-        
-        // Test sur l'ouverture du fichier des logins
-        if((users = fopen(ufname,"r"))==NULL)
-                {perror("Echec fopen:"); return -1;}
-        
+  ufname = (char*)malloc(sizeof "users");
+  strcpy(ufname,"users");
 
-        #ifdef DEBUG
-        printf("======= Fin Lecture ======");
-        #endif
+  /* Lecture des arguments de la commande */
+#ifdef DEBUG
+  printf("======= Lecture ======");
+#endif
 
-	/* Initialisation du serveur */
-	ecoute = initialisationServeur(&nport);
-		
-	/* Lancement de la boucle d'ecoute */
-	//if(boucleServeur(ecoute,dummy,gestionClient)==-1) perror("Echec serveur");
-	if(boucleServeur(ecoute,lanceClientLeger,gestionClient)==-1) perror("Echec serveur");
+  // On verifie que le arguments sont bons
+  if((analyseArg(argc,argv,&nport,ufname))==-1)
+    {display_right_syntax(); return -1;}
 
-        fclose(users);
-        free(ufname);
+  // Test sur l'ouverture du fichier des logins
+  if((users = fopen(ufname,"r"))==NULL)
+    {perror("Echec fopen:"); return -1;}
 
-        return 0;
+
+#ifdef DEBUG
+  printf("======= Fin Lecture ======");
+#endif
+
+  /* Creation du process */
+  printf("Entrez le nom du fichier à parser:");
+  scanf("%s",uxml);
+  if(addProcess(&t_process, uxml)!=0)
+    {printf("Erreur de parsing fichier %s",uxml); return -1;}
+
+  /* Initialisation du serveur */
+  ecoute = initialisationServeur(&nport);
+
+  /* Lancement de la boucle d'ecoute */
+  //if(boucleServeur(ecoute,dummy,gestionClient)==-1) perror("Echec serveur");
+  if(boucleServeur(ecoute,lanceClientLeger,gestionClient)==-1) 
+    {perror("Echec serveur"); return -1;}
+
+  fclose(users);
+  free(ufname);
+
+  return 0;
 }
